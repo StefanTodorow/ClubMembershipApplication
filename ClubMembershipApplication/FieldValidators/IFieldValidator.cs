27882371a9854ed2cfb18dfba3c01971b09a8f16
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-08-18 18:03:12 +0300

    Created Field Validator interface

---
+++ ClubMembershipApplication/FieldValidators/IFieldValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication.FieldValidators
+{
+    public delegate bool FieldValidatorDel(int fieldIndex, string fieldValue, string[] fieldArray, out string fieldInvalidMessage);
+    public interface IFieldValidator
+    {
+        void InitializeValidatorDelegates();
+
+        string[] FieldArray { get; }
+
+        FieldValidatorDel ValidatorDel { get; }
+    }
+}
