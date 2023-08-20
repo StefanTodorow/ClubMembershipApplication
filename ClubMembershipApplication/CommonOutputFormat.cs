052b8ba43062c54e3c5e4e3a0fe85d252a3dfeec
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-08-20 17:02:33 +0300

    Created CommonOutputFormat class for console font themes

---
+++ ClubMembershipApplication/CommonOutputFormat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication
+{
+    public static class CommonOutputFormat
+    {
+        public enum FontTheme
+        {
+            Default,
+            Danger,
+            Success
+        }
+        public static void ChangeFontColor(FontTheme fontTheme)
+        {
+            switch (fontTheme)
+            {
+                case FontTheme.Danger:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case FontTheme.Success:
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                default:
+                    Console.ResetColor();
+                    break;
+            }
+        }
+    }
+}
