7efef36f936e2f3b45d9bf1dcb34f879f066be1f
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-08-22 17:20:05 +0300

    Added RunView of mainView in the Main method

--- ClubMembershipApplication/Program.cs
+++ ClubMembershipApplication/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace ClubMembershipApplication
+﻿using ClubMembershipApplication.Views;
+
+namespace ClubMembershipApplication
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IView mainView = Factory.GetMainViewObject();
+            mainView.RunView();
+
+            Console.ReadKey();
         }
     }
 }
